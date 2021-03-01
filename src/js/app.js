64d17d1b0e66878f8432212c9516beca7719b33f
直也テック <58985013+coding-youtuber@users.noreply.github.com>
2021-03-01 12:50:38 +0900

    add 空文についてのコメント

--- src/js/app.js
+++ src/js/app.js
@@ -12,6 +12,24 @@ document.body.appendChild(c);
 var perm = [];
 
 while (perm.length < 255) {
+  /*
+  参考
+
+  ・空文について
+  空文 - JavaScript | MDN
+  https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Statements/Empty
+  
+  while文の中身はない。中身がないので空文となる。
+  
+  ・やりたいこと
+  配列permには重複なしの0~255の整数を入れたい。includesによって重複チェックをしている。
+  
+  ・includesの返り値がtrue、つまりランダムに生成した整数が既存の配列permのいずれかの要素と重複していた
+  このときwhile文の条件式がtrueになるのでもう一度整数の乱数が生成されincludesによる重複チェックが行われる
+  
+  ・includesの返り値がfalse、つまりランダムに生成した整数が既存の配列permのすべての要素と重複していなかった
+  このときwhile文は終了する。while文の条件式で使ったvalは生きている。なのでvalを配列permに新規追加している。
+  */
   while (perm.includes(val = Math.floor(Math.random() * 255)));
   perm.push(val);
 }
